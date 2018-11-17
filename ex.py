cd15b2c8d7e9260f5d2c0b128eb5c4b0ac6b7ded
Branlam97 <40681500+Branlam97@users.noreply.github.com>
2018-11-17 11:59:29 -0800

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,14 @@
+'''
+Created on Nov 15, 2018
+
+@author: brand
+'''
+import numpy as np
+import math as m
+print('Hello World')
+for x in range(10):
+    print('Hello world')
+
+a = np.array([1, 2, 3])
+print(a)
+print(m.factorial(3))
